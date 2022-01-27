cd3e4f1c615105a3d19d31dc7c53cafd251738e1
SumitK27 <sumitkajbaje@gmail.com>
2022-01-27 13:55:58 +0530

    Dsiplayed Response Headers

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import prettyBytes from "pretty-bytes";
 
 import axios from "axios";
 
@@ -9,6 +10,9 @@ const requestHeadersContainer = document.querySelector(
     "[data-request-headers]"
 );
 const keyValueTemplate = document.querySelector("[data-key-value-template]");
+const responseHeadersContainer = document.querySelector(
+    "[data-response-headers]"
+);
 
 document
     .querySelector("[data-query-param-btn]")
@@ -24,6 +28,23 @@ document
 queryParamsContainer.append(createKeyValuePair());
 requestHeadersContainer.append(createKeyValuePair());
 
+axios.interceptors.request.use((request) => {
+    request.customData = request.customData || {};
+    request.customData.startTime = new Date().getTime();
+    return request;
+});
+
+function updateEndTime(response) {
+    response.customData = response.customData || {};
+    response.customData.time =
+        new Date().getTime() - response.config.customData.startTime;
+    return response;
+}
+
+axios.interceptors.response.use(updateEndTime, (e) => {
+    return Promise.reject(updateEndTime(e.response));
+});
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -32,9 +53,17 @@ form.addEventListener("submit", (e) => {
         method: document.querySelector("[data-method]").value,
         params: keyValuePairsToObjects(queryParamsContainer),
         headers: keyValuePairsToObjects(requestHeadersContainer),
-    }).then((response) => {
-        console.log(response);
-    });
+    })
+        .catch((e) => e)
+        .then((response) => {
+            document
+                .querySelector("[data-response]")
+                .classList.remove("d-none");
+            updateResponseDetails(response);
+            // updateResponseEditor(response.data);
+            updateResponseHeaders(response.headers);
+            console.log(response);
+        });
 });
 
 function createKeyValuePair() {
@@ -56,4 +85,28 @@ function keyValuePairsToObjects(container) {
         if (key === "") return data;
         return { ...data, [key]: value };
     }, {});
+}
+
+function updateResponseHeaders(headers) {
+    responseHeadersContainer.innerHTML = "";
+    Object.entries(headers).forEach(([key, value]) => {
+        // Add Key
+        const keyElement = document.createElement("div");
+        keyElement.textContent = key;
+        responseHeadersContainer.appendChild(keyElement);
+        // Add Value
+        const valueElement = document.createElement("div");
+        valueElement.textContent = value;
+        responseHeadersContainer.appendChild(valueElement);
+    });
+}
+
+function updateResponseDetails(response) {
+    document.querySelector("[data-status]").textContent = response.status;
+    document.querySelector("[data-time]").textContent =
+        response.customData.time;
+    document.querySelector("[data-size]").textContent = prettyBytes(
+        JSON.stringify(response.data).length +
+            JSON.stringify(response.headers).length
+    );
 }
